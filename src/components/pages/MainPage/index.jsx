526050acbcd556e443cab4e6d7a763823e7dd7d0
liubaterehova <liubov.terehova@gmail.com>
2019-12-06 14:03:55 +0300

    в

--- src/components/pages/MainPage/index.jsx
+++ src/components/pages/MainPage/index.jsx
@@ -2,20 +2,16 @@ import React, { Component } from "react";
 import SearchPanel from "../../moleculas/SearchPanel";
 import SortButtons from "../../moleculas/SortButtons";
 import ListCharacters from "../../organisms/ListCharacters";
+import Template from "../../Template";
 
 export default class MainPage extends Component {
   render() {
     return (
-      <div
-        style={{
-          width: "700px",
-          margin: "0 auto"
-        }}
-      >
+      <Template>
         <SearchPanel />
         <SortButtons />
         <ListCharacters />
-      </div>
+      </Template>
     );
   }
 }
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import MainPage from '../components/pages/MainPage';
+
+export const customerRoutes = [{
+    path: "/main",
+    component: MainPage,
+    exact: true
+}, ]
